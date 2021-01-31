afa37cea4ddcabbf9f55878ac2655c18ec982b7f
r.kalinin <fabulous.ept@gmail.com>
2021-01-31 20:06:25 +0300

    deleted vue.config.js

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    pages: {
-        index: {
-            // точка входа для страницы
-            entry: 'frontend/main.js',
-        }
-    },
-    outputDir: 'public/build'
-}
